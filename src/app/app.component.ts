ab18df3e1b57326fd48406f723b97b1e69a8009e
Josue Cano <jdevelopcano@gmail.com>
2023-07-15 22:02:31 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Menu, MenuModule } from 'primeng/menu';
+
+@NgModule({
+  exports: [MenuModule],
+})
+export class PrimeNgModule {}
